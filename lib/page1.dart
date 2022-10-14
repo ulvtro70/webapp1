2fb8187cba15ce59823c72b9e7cf52daab63391b
ulvtro70 <115401186+ulvtro70@users.noreply.github.com>
2022-10-14 09:43:53 +0200

    Lagt til kart

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
@@ -7,9 +9,24 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Page1'),
+        title: const Text('Map'),
       ),
-      body: const Center(child: Text('Data page 1')),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(42.58, 12.43),
+          zoom: 6,
+        ),
+        children: [
+          TileLayer(
+            wmsOptions: WMSTileLayerOptions(
+              baseUrl: 'https://{s}.s2maps-tiles.eu/wms/?',
+              layers: ['s2cloudless-2018_3857'],
+            ),
+            subdomains: const ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+          )
+        ],
+      ),
     );
   }
 }
